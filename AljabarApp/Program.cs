7b35716b1f493b64aa4e9956e5d8f2d18d6bc0ca
IksanRisandy <iksanrisandy@gmail.com>
2024-05-05 20:50:13 +0700

    membuat library AljabarLibraries dan implementasinya pada AljabarApp

---
+++ AljabarApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AljabarApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double[] persamaan = { 1, -3, -10 };
+            Console.WriteLine($"Mencari akar persamaan kuadrat dari {persamaan[0]}x^2 {persamaan[1]}x {persamaan[2]}");
+            double[] akar = AljabarLibraries.Aljabar.AkarPersamaanKuadrat(persamaan);
+            Console.WriteLine($"Akar persamaan kuadrat: {akar[0]} dan {akar[1]}");
+
+            double[] persamaanLinear = { 2, -3 };
+            double[] hasilKuadrat = AljabarLibraries.Aljabar.HasilKuadrat(persamaanLinear);
+            Console.WriteLine($"\nMencari hasil kuadrat dari persamaan {persamaanLinear[0]}x {persamaanLinear[1]}");
+            Console.WriteLine($"Hasil kuadrat: {hasilKuadrat[0]}x^2 + {hasilKuadrat[1]}x + {hasilKuadrat[2]}");
+        }
+    }
+}
---
+++ AljabarLibraries/Aljabar.cs
@@ -0,0 +1,32 @@
+﻿namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double discriminant = (b*b) - (4*a*c);
+
+            if (discriminant < 0)
+            {
+                throw new Exception("Akar persamaan kuadrat bukan bilangan real");
+            } 
+
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2*a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            return new double[] { (a * a), (2 * a * b), (b * b) };
+        }
+    }
+}
